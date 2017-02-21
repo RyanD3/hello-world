6f1fbcd628fcc18c60f48201367c3c0455f94a87
RyanD3 <RyanD3@users.noreply.github.com>
2017-02-21 15:48:38 +1100

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,11 @@
+
+* i plan to make a text adventure game in python this is just a quick idea of the beggining sequence what would you start with?
+*
+
+*
+you wake up in a prison on a unkown swamp planet it is a rudimentarty prison with dirt on the floor and dirty gray walls
+a voice sounds over the intercom asking for your name and skills*
+
+print("welcome to the game")
+input_var = var("what is your name?")
+*anything i sould do next?*
